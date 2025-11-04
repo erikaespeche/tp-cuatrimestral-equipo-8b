1bf42e13c0eac12e277317cbdd6636b5d170bcad
jooseaguirre <jooseaguirre08@gmail.com>
2025-11-04 16:58:34 -0300

    Ventana login y perfil adm

--- ClinicaWeb/Login.aspx.cs
+++ ClinicaWeb/Login.aspx.cs
@@ -7,18 +7,16 @@ namespace Clinic
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // Se ejecuta cada vez que se carga la página.
-            // Ideal para inicializar controles o limpiar mensajes.
+            
             if (!IsPostBack)
             {
-                // Código de inicialización (vacío por ahora)
+                
             }
         }
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
-            // Evento del botón "Ingresar".
-            // Acá más adelante se agregará la lógica de autenticación.
+            
         }
     }
 }
