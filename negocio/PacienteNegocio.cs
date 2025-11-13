20007972e49e336dbeb2eff2930d29a5dbdd9782
Maia <maia-nair@hotmail.com>
2025-11-12 23:53:56 -0300

    cree la clase AccesoDatos para centralizar la conexión y agregue nuevos metodos a PacienteNegocio

--- negocio/PacienteNegocio.cs
+++ negocio/PacienteNegocio.cs
@@ -14,39 +14,32 @@ namespace negocio
         public List<Paciente> Listar()
         {
             List<Paciente> listaPacientes = new List<Paciente>();
-            SqlConnection conexion = new SqlConnection();
-            SqlCommand comandoPaciente = new SqlCommand();
-            SqlDataReader lectorPaciente;
+            AccesoDatos datos = new AccesoDatos();
 
             try
             {
-                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CLINICA_DB; integrated security=true";
-                conexion.Open();
-
-                comandoPaciente.CommandType = System.Data.CommandType.Text;
-                comandoPaciente.CommandText = @"SELECT IdPaciente, DniPaciente, Nombres, Apellidos, 
+                datos.setearConsulta (@"SELECT IdPaciente, DniPaciente, Nombres, Apellidos, 
                                                 FechaNacimiento, Sexo, GrupoSanguineo, Email, Telefono, Celular, Direccion, 
-                                                Ciudad, Provincia, CodigoPostal FROM PACIENTES";
-                comandoPaciente.Connection = conexion;
+                                                Ciudad, Provincia, CodigoPostal FROM PACIENTES");
+                datos.ejecutarLectura();
 
-                lectorPaciente = comandoPaciente.ExecuteReader();
-                while (lectorPaciente.Read())
+                while (datos.Lector.Read())
                 {
                     Paciente aux = new Paciente();
-                    aux.IdPaciente = (int)lectorPaciente["IdPaciente"];
-                    aux.DniPaciente = (int)lectorPaciente["DniPaciente"];
-                    aux.Nombres = lectorPaciente["Nombres"].ToString();
-                    aux.Apellidos = lectorPaciente["Apellidos"].ToString();
-                    aux.FechaNacimiento = (DateTime)lectorPaciente["FechaNacimiento"];
-                    aux.Sexo = char.Parse(lectorPaciente["Sexo"].ToString());
-                    aux.GrupoSanguineo = lectorPaciente["GrupoSanguineo"].ToString();
-                    aux.Email = lectorPaciente["Email"].ToString();
-                    aux.Telefono = lectorPaciente["Telefono"].ToString();
-                    aux.Celular = lectorPaciente["Celular"].ToString();
-                    aux.Direccion = lectorPaciente["Direccion"].ToString();
-                    aux.Ciudad = lectorPaciente["Ciudad"].ToString();
-                    aux.Provincia = lectorPaciente["Provincia"].ToString();
-                    aux.CodigoPostal = lectorPaciente["CodigoPostal"].ToString();
+                    aux.IdPaciente = (int)datos.Lector["IdPaciente"];
+                    aux.DniPaciente = (int)datos.Lector["DniPaciente"];
+                    aux.Nombres = datos.Lector["Nombres"].ToString();
+                    aux.Apellidos = datos.Lector["Apellidos"].ToString();
+                    aux.FechaNacimiento = (DateTime)datos.Lector["FechaNacimiento"];
+                    aux.Sexo = char.Parse(datos.Lector["Sexo"].ToString());
+                    aux.GrupoSanguineo = datos.Lector["GrupoSanguineo"].ToString();
+                    aux.Email = datos.Lector["Email"].ToString();
+                    aux.Telefono = datos.Lector["Telefono"].ToString();
+                    aux.Celular = datos.Lector["Celular"].ToString();
+                    aux.Direccion = datos.Lector["Direccion"].ToString();
+                    aux.Ciudad = datos.Lector["Ciudad"].ToString();
+                    aux.Provincia = datos.Lector["Provincia"].ToString();
+                    aux.CodigoPostal = datos.Lector["CodigoPostal"].ToString();
 
                     listaPacientes.Add(aux);
                 }
@@ -59,5 +52,100 @@ namespace negocio
 
             return listaPacientes;
         }
+            public int Agregar(Paciente nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta(@"INSERT INTO PACIENTES 
+                    (DniPaciente, Nombres, Apellidos, FechaNacimiento, Sexo, GrupoSanguineo,
+                     Email, Telefono, Celular, Direccion, Ciudad, Provincia, CodigoPostal)
+                    VALUES (@dni, @nombres, @apellidos, @fecha, @sexo, @grupo,
+                            @mail, @tel, @cel, @dir, @ciudad, @prov, @cp)");
+
+                datos.setearParametro("@dni", nuevo.DniPaciente);
+                datos.setearParametro("@nombres", nuevo.Nombres);
+                datos.setearParametro("@apellidos", nuevo.Apellidos);
+                datos.setearParametro("@fecha", nuevo.FechaNacimiento);
+                datos.setearParametro("@sexo", nuevo.Sexo);
+                datos.setearParametro("@grupo", nuevo.GrupoSanguineo);
+                datos.setearParametro("@mail", nuevo.Email);
+                datos.setearParametro("@tel", nuevo.Telefono);
+                datos.setearParametro("@cel", nuevo.Celular);
+                datos.setearParametro("@dir", nuevo.Direccion);
+                datos.setearParametro("@ciudad", nuevo.Ciudad);
+                datos.setearParametro("@prov", nuevo.Provincia);
+                datos.setearParametro("@cp", nuevo.CodigoPostal);
+
+                return datos.obtenerId();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al agregar paciente: " + ex.Message);
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void Modificar(Paciente paciente)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta(@"UPDATE PACIENTES SET 
+                        DniPaciente=@dni, Nombres=@nombres, Apellidos=@apellidos, 
+                        FechaNacimiento=@fecha, Sexo=@sexo, GrupoSanguineo=@grupo,
+                        Email=@mail, Telefono=@tel, Celular=@cel, Direccion=@dir,
+                        Ciudad=@ciudad, Provincia=@prov, CodigoPostal=@cp
+                    WHERE IdPaciente=@id");
+
+                datos.setearParametro("@id", paciente.IdPaciente);
+                datos.setearParametro("@dni", paciente.DniPaciente);
+                datos.setearParametro("@nombres", paciente.Nombres);
+                datos.setearParametro("@apellidos", paciente.Apellidos);
+                datos.setearParametro("@fecha", paciente.FechaNacimiento);
+                datos.setearParametro("@sexo", paciente.Sexo);
+                datos.setearParametro("@grupo", paciente.GrupoSanguineo);
+                datos.setearParametro("@mail", paciente.Email);
+                datos.setearParametro("@tel", paciente.Telefono);
+                datos.setearParametro("@cel", paciente.Celular);
+                datos.setearParametro("@dir", paciente.Direccion);
+                datos.setearParametro("@ciudad", paciente.Ciudad);
+                datos.setearParametro("@prov", paciente.Provincia);
+                datos.setearParametro("@cp", paciente.CodigoPostal);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al modificar paciente: " + ex.Message);
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void Eliminar(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("DELETE FROM PACIENTES WHERE IdPaciente = @id");
+                datos.setearParametro("@id", id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al eliminar paciente: " + ex.Message);
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
+
 }
